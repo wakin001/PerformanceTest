491c5465155ba2f60a25b8da1ba3fb50194c1aab
Wang Feng <wang_feng@cygames.co.jp>
2017-04-07 18:34:31 +0900

    Added TestGetComponent

---
+++ Assets/Scripts/CustomTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Diagnostics;
+
+public class CustomTimer : IDisposable 
+{
+    private string _timerName;
+    private int _numTests;
+    private Stopwatch _watch;
+
+    public CustomTimer(string timerName, int numTests)
+    {
+        _timerName = timerName;
+        _numTests = numTests;
+
+        if (_numTests <= 0)
+        {
+            _numTests = 1;
+        }
+        _watch = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+        _watch.Stop();
+        float ms = _watch.ElapsedMilliseconds;
+        UnityEngine.Debug.Log(string.Format("{0} finished : {1:0.00} ms total {2:0.000000} ms per test for {3} tests", 
+            _timerName, ms, ms / _numTests, _numTests));
+    }
+}
---
+++ Assets/Scripts/TestGetComponent.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Result:
+/// GetComponent(string) finished : 1886.00 ms total 0.001886 ms per test for 1000000 tests
+/// GetComponent<ComponentName>() finished : 79.00 ms total 0.000079 ms per test for 1000000 tests   (Fastest!! Unity 5.4)
+/// GetComponent(typof(ComponentName)) finished : 82.00 ms total 0.000082 ms per test for 1000000 tests
+/// </summary>
+public class TestGetComponent : MonoBehaviour 
+{
+    int numTests = 1000000;
+    TestComponent test;
+
+	// Use this for initialization
+	void Start () 
+    {
+        using (new CustomTimer("GetComponent(string)", numTests))
+        {
+            for (var i = 0; i < numTests; ++i)
+            {
+                test = (TestComponent)GetComponent("TestComponent");
+            }
+        }
+
+        using (new CustomTimer("GetComponent<ComponentName>()", numTests))
+        {
+            for (var i = 0; i < numTests; ++i)
+            {
+                test = GetComponent<TestComponent>();
+            }
+        }
+
+        using (new CustomTimer("GetComponent(typof(ComponentName))", numTests))
+        {
+            for (var i = 0; i < numTests; ++i)
+            {
+                test = (TestComponent)GetComponent(typeof(TestComponent));
+            }
+        }
+
+	}
+	
+}
